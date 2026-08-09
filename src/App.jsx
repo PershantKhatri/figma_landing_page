96b2722e6f882142a384bf3a9ac7fd287849fe6f
Pershant0011 <kparshant525@gmail.com>
2026-08-09 17:34:33 +0500

    assignment

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/figma_landing_page">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
